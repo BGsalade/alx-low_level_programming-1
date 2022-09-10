18ae0f285a5be7f69cdefca4042b827236fe39bf
iketimi <alaweitimi@gmail.com>
2022-09-09 17:02:32 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * * main - entry point
+ * * Return: always 0 (success)
+ */
+int main(void)
+{
+char c = 'z';
+for (; c >= 'a'; c--)
+putchar (c);
+putchar ('\n');
+return (0);
+}
