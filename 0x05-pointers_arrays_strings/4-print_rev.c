92429ac44e03181141128850093d5ee8c12c57e5
iketimi <alaweitimi@gmail.com>
2022-09-19 06:18:20 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_rev - Function that prints a string in reverse
+*@s: String in reverse
+*Return: nothing
+*/
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len--)
+		putchar(*(s + len));
+	putchar(10);
+}
