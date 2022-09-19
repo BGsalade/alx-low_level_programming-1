a940de5761833929f488a3d95b44008b2e6dba3a
iketimi <alaweitimi@gmail.com>
2022-09-19 11:15:38 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,15 +2,16 @@
 /**
  * rev_string - Function to reverse the string
  * @s: pointer to string
+ *
  * Return: nothing
  */
 
 void rev_string(char *s)
 {
-	int i, j, k, z;
+	int i, j, k, tmp;
 
 	i = 0;
-	while (s(i) != '\0')
+	while (; s[i] != '\0')
 	{
 	i++;
 	}
@@ -20,9 +21,9 @@ void rev_string(char *s)
 	while (k < j)
 	
 	{
-	z = s(k);
-	s(k) = s(j);
-	s(j) = z;
+	tmp = s[k];
+	s[k] = s[j];
+	s[j] = z;
 	k++;
 	j--;
 	}
