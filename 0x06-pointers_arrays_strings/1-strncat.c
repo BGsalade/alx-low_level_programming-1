8d5feb835fb79fb7ac47daa1e096c49fe88bce84
iketimi <alaweitimi@gmail.com>
2022-09-22 05:08:29 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,14 +6,18 @@
  *@n: integer parameter to compare index
  *Return: new concatenated strings
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
 	
-	while (dest[i++])
-		j++;
+	int index = 0, dest_len = 0;
 	
-	for (i = 0; src[i] && i < n; i++)
-		dest[j++] = src[i];
+	while (dest[index++])
+		dest_len++;
+	
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	
 	return (dest);
+
 }
