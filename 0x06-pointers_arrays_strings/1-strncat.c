dd6217d952c491f2c6269b31c70110da5fa9c33c
iketimi <alaweitimi@gmail.com>
2022-09-22 10:37:56 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * * _strncat - two words
  * @dest : pointer to char param
@@ -7,27 +6,23 @@
  * @n : int parameter
  * Return: *dest
  */
-
 char *_strncat(char *dest, char *src, int n)
 {
-	int m;
-	int i;
-
-	m = 0;
-	
-	for (i = 0; i < 1000; i++)
-	{
-		if (dest[i] == '\0')
-		{
-			break;
-		}
-		m++;
-	}
-
-	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[m + i] = src[i];
-	}
-	dest[m + i] = src[i];
-	return (dest);
+int m;
+int i;
+m = 0;
+for (i = 0; i < 1000; i++)
+{
+if (dest[i] == '\0')
+{
+break;
+}
+m++;
+}
+for (i = 0; src[i] != '\0' && i < n; i++)
+{
+dest[m + i] = src[i];
+}
+dest[m + i] = src[i];
+return (dest);
 }
