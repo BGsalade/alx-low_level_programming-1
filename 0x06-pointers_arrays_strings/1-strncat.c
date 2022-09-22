957536b2a92b7ceaf4bcf3672797e57c77c21206
iketimi <alaweitimi@gmail.com>
2022-09-22 04:57:21 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,4 @@
-#include <main.h>
-#include <stdio.h>
+#include "main.h"
 /**
  *_strncat - concatenate two strings inputting number of bytes
  *@dest: string to append upon
@@ -10,13 +9,11 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, j = 0;
-
+	
 	while (dest[i++])
 		j++;
-
+	
 	for (i = 0; src[i] && i < n; i++)
 		dest[j++] = src[i];
-
 	return (dest);
-
 }
