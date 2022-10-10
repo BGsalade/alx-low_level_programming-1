0ee7bb64d43c82aa60d3f5ed6f4d94fa5bfe0942
TiO-DevOps <timoyinpere@gmail.com>
2022-10-11 00:03:17 +0100

    C-Structures Typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H_FILE
-#define DOG_H_FILE
+#ifndef STRTYP
+#define STRTYP
 
 /**
  * struct dog - structure definition of a dog
@@ -7,15 +7,17 @@
  * @age: dog age
  * @owner: owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+}
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+#endif /* STRTYP */
